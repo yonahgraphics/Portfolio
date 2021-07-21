983e3b23e58d2acdfedf258d5b39630fff08ebb0
yonahgraphics <yonahgraphics@gmail.com>
2021-07-21 19:47:15 +0200

    Added the web folder

--- lib/presentation/widgets/navigation_bar/nav_item.dart
+++ lib/presentation/widgets/navigation_bar/nav_item.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-class NavItem extends StatelessWidget {
+class NavItem extends StatefulWidget {
    String title;
    NavItem({required this.title});
 
   @override
+  _NavItemState createState() => _NavItemState();
+}
+
+class _NavItemState extends State<NavItem> {
+   bool _isHover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Text(title, style: GoogleFonts.montserrat(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold));
+    return MouseRegion(
+        onEnter: (f)=>setState(()=>_isHover = true),
+        onExit: (f)=>setState(()=>_isHover = false),
+        child: Text(widget.title, style: GoogleFonts.montserrat(fontSize: _isHover?40:15, color: _isHover?Colors.pink:Colors.white, fontWeight: FontWeight.bold)));
   }
 }
